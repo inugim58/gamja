ac23c3ddf891fcc9ae89a2f0281ff4ac0bc3d56d
inugim58 <inugim58@gmail.com>
2025-06-08 15:40:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-$("nav").hover(
-    function() {
-        $(".submenu").slideDown("slow");
-        $(".menubg").slideDown("slow");
-    },
-    function() {
-        $(".submenu").slideUp("slow")
-        $(".menubg").slideUp("slow")
-    }
-);
-var slider = $(".slide a");
-var sno = 0;
-function playSlide(){
-    $(slider[sno]).animate( {left:"100%"},2000,
-    function() { $(this).css({left:"-100%"});
-});
-sno++;
-if(sno>2) sno=0;
-$(slider[sno]).animate({left:"0"},2000);
-}
-setInterval(playSlide,4000);
-
 function openPop() {
     $(".pop").show();
 }
